68c5b336ba16cc8fbd80f8a35dad8fb77788636a
Prabhat Shaw <prabhat.shaw77@gmail.com>
2021-08-20 14:31:42 +0530

    added more configuration for eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,23 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
+    'eslint:recommended', // recommended rules for eslint
+    'plugin:@typescript-eslint/recommended', // recommended rules for typescript eslint
+    'airbnb-base', // airbnb rules
+    'plugin:prettier/recommended', // prettier recommended rules
+    'plugin:import/errors', // error rules
+    'plugin:import/warnings', // warnings rules
+    'plugin:import/typescript', // typescript rules
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
   rules: {
+    'prettier/prettier': 'error', // show error for prettier rules
+    'import/extensions': 'off', // switch off files extension during import
+    'no-console': 'off',
   },
 };
