2f5080ffe9c69e9df8bf627b7592b6794e9e13fc
Prabhat Shaw <prabhat.shaw77@gmail.com>
2021-08-20 17:55:04 +0530

    added config for import order and import resolver (absolute path)

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,27 @@ module.exports = {
     'prettier/prettier': 'error', // show error for prettier rules
     'import/extensions': 'off', // switch off files extension during import
     'no-console': 'off',
+    'import/order': [
+      // rule for grouping and maintain order import
+      'error',
+      {
+        'newlines-between': 'never',
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index'],
+        ],
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code
+        project: './tsconfig.json',
+      },
+    },
   },
 };
